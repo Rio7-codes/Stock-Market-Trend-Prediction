f6dbcde3d297b4e7ee412fb608fac3b7e05eb4f9
Souhardya Mridha <s2004indiam@gmail.com>
2025-07-06 01:58:37 +0530

    Delete src directory

--- src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { useAuth } from "../auth/AuthContext";
-import { Navigate } from "react-router-dom";
-
-export function ProtectedRoute({ children }) {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" />;
-}
